1fdf94e6314c22823c18d288704bb46a7c75d3f8
Milton Lima <milton.lima@outlook.com>
2020-05-20 09:48:49 -0300

    Criado metodo calc_bonus e aplicado calculo

--- python-6/main.py
+++ python-6/main.py
@@ -4,38 +4,36 @@ class Department:
     def __init__(self, name, code):
         self.name = name
         self.code = code
-
-
+    
 class Employee:
-    def __init__(self, code, name, salary):
-        self.code = code
-        self.name = name
-        self.salary = salary
-
-    def calc_bonus(self):
-        pass
-
-    def get_hours(self):
-        pass
-
+    work_hours = 8
+    def __init__(self, code, name, salary, department):
+        self._code = code
+        self._name = name
+        self._salary = salary
+        self.salary = department
 
 class Manager(Employee):
-    def __init__(self, code, name, salary):
+    def __init__(self, code, name, salary, department):
         super().__init__(code, name, salary)
         self.departament = Department('managers', 1)
 
     def calc_bonus(self):
         return self.salary * 0.15
-    
-    def get_department(self, ):
-        department.name = name
-        pass
-    
-class Seller(Manager):
-    def __init__(self, code, name, salary):
+
+class Seller(Employee):
+    def __init__(self, code, name, salary, sales, departament):
         super().__init__(code, name, salary)
         self.departament = Department('sellers', 2)
-        self.sales = 0
+        self._sales += 1
+    
+    @staticmethod
+    def get_sales(self):
+        self.get_sales = self._sales
+    
+    def put_sales(self):
+        self.put_sales += self._sales
 
-    def get_hours(self):
-        return 6
+    def calc_bonus(self):
+        self.cal_bonus = get_sales * 0.15
+    
