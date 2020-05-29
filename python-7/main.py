d19166816b551883699a9d7232776f8f1183348d
Milton Lima <milton.lima@outlook.com>
2020-05-29 10:09:52 -0300

    Criado arquivo de testes com monkeypacth - Create tests files with monkeypatch

---
+++ python-7/main.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def get_temperature(lat, lng):
+    key = 'e1ee55658d4a2b28c4841e373c3b3d87'
+    url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
+    reponse = requests.get(url)
+    data = reponse.json()
+    temperature = data.get('currently').get('temperature')
+    print(data)
+    if not temperature:
+        return
+    return int((temperature - 32) * 5.0 / 9.0)
+    
